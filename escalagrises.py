374066fbc9b5312abbb8f5c162f595240b14fe2e
knight19720208ui <20604988+knight19720208ui@users.noreply.github.com>
2021-11-16 17:25:27 -0500

    Create escalagrises.py
    Versión de consola

---
+++ escalagrises.py
@@ -0,0 +1,15 @@
+'''
+Verisón de consola
+'''
+import cv2 
+  
+image = cv2.imread('leo.png')  # drive:\\path\\file
+cv2.imshow('El Original', image) 
+cv2.waitKey() 
+  
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+  
+cv2.imshow('En escala grises', gray_image) 
+cv2.waitKey(0)   
+  
+cv2.destroyAllWindows() 
